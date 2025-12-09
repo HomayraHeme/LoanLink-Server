91fe0a4133aed1de35ca7b1ef075f0c4a2175c56
HomayraHeme <heme5674@gmail.com>
2025-12-09 14:17:30 +0600

    profile page added

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const crypto = require('crypto');
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./loanlink-firebase-adminsdk.json");
+// const serviceAccount = require("./loanlink-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
